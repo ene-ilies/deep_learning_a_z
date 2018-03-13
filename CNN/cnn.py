fc53e7cdf63f74182d706095ad50f48a75542350
Bogdan Ilies <ene.ilies@gmail.com>
2018-03-13 22:55:21 +0200

    Improved model.
    Signed-off-by: Bogdan Ilies <ene.ilies@gmail.com>

--- CNN/cnn.py
+++ CNN/cnn.py
@@ -8,29 +8,34 @@ Created on Sun Mar 11 12:39:09 2018
 
 # Building the CNN
 from keras.models import Sequential
-from keras.layers import Convolution2D
+from keras.layers import Conv2D
 from keras.layers import MaxPooling2D
 from keras.layers import Flatten
 from keras.layers import Dense
+from keras.layers import Dropout
 from keras import backend
 
 # Building the CNN
 classifier = Sequential()
 
 # Adding convolution layer
-classifier.add(Convolution2D(32, 3, 3, input_shape=(64, 64, 3), activation='relu'))
+classifier.add(Conv2D(32, (3, 3), input_shape=(128, 128, 3), activation='relu'))
 # Adding max pooling layer
 classifier.add(MaxPooling2D(pool_size=(2, 2)))
 
-classifier.add(Convolution2D(32, 3, 3, activation='relu'))
+classifier.add(Conv2D(32, (3, 3), activation='relu'))
+classifier.add(MaxPooling2D(pool_size=(2, 2)))
+
+classifier.add(Conv2D(64, (3, 3), activation='relu'))
 classifier.add(MaxPooling2D(pool_size=(2, 2)))
 
 # Flattening
 classifier.add(Flatten())
 
 # Fully Connected
-classifier.add(Dense(output_dim=128, activation='relu'))
-classifier.add(Dense(output_dim=1, activation='sigmoid'))
+classifier.add(Dense(units=64, activation='relu'))
+classifier.add(Dropout(0.5))
+classifier.add(Dense(units=1, activation='sigmoid'))
 
 # Compiling
 classifier.compile(optimizer='adam', loss='binary_crossentropy', metrics=['accuracy'])
@@ -48,33 +53,33 @@ test_datagen = ImageDataGenerator(rescale=1./255)
 
 training_set = train_datagen.flow_from_directory(
         'dataset/training_set',
-        target_size=(64, 64),
+        target_size=(128, 128),
         batch_size=32,
         class_mode='binary')
 
 test_set = test_datagen.flow_from_directory(
         'dataset/test_set',
-        target_size=(64, 64),
+        target_size=(128, 128),
         batch_size=32,
         class_mode='binary')
 
 classifier.fit_generator(
         training_set,
-        samples_per_epoch=8000,
-        nb_epoch=25,
+        steps_per_epoch=8000/32,
+        epochs=50,
         validation_data=test_set,
-        nb_val_samples=800)
+        validation_steps=800/32)
 
 real_img_gen = ImageDataGenerator(rescale=1./255)
 
 image_generator = real_img_gen.flow_from_directory(
 	'dataset/single_prediction',
-	target_size=(64,64),
+	target_size=(128, 128),
 	class_mode=None,
 	shuffle=False)
 
 real_image_pred = classifier.predict_generator(image_generator,
-	val_samples=2)
+	steps=1)
 
 backend.clear_session()
 
